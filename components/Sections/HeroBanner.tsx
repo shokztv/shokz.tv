64033bc6bf4f957c8002dbedc0d652fca450a55d
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2021-09-10 13:46:56 +0200

    Removed git action, formatting;

--- components/Sections/HeroBanner.tsx
+++ components/Sections/HeroBanner.tsx
@@ -1,108 +1,122 @@
 import React, { ReactElement, useCallback } from "react";
+
 import Discord from "../Icons/Discord";
+import Image from "next/image";
 import Instagram from "../Icons/Instagram";
 import Twitch from "../Icons/Twitch";
 import Twitter from "../Icons/Twitter";
-import Image from 'next/image';
 
 export default function HeroBanner(): ReactElement {
-    const scrollTo = useCallback((id: string) => document.getElementById(id).scrollIntoView({ behavior: 'smooth' }), []);
+  const scrollTo = useCallback(
+    (id: string) =>
+      document.getElementById(id).scrollIntoView({ behavior: "smooth" }),
+    []
+  );
 
-    return <div className={'heroBanner'}>
-        <div className={'inner'}>
-            <div className={'logo'}>
-                <Image width={280} src={'/images/herobanner_logo.png'} height={120} alt={'ShokzTV Logo'} />
-            </div>
-
-            <div className={'heroInfo'}>
-                <h1>shokzTV</h1>
-                <h2>Dota 2-Kommentatorenkanal</h2>
-                <div className={'socials'}>
-                    <div className={'link twitter'}>
-                        <Twitter link={'https://twitter.com/shokzTV'} />
-                    </div>
-                    <div className={'link twitch'}>
-                        <Twitch link={'https://twitch.tv/shokzTV'} />
-                    </div>
-                    <div className={'link discord'}>
-                        <Discord link={'https://discord.gg/hagYNWg'} />
-                    </div>
-                    <div className={'link instagram'}>
-                        <Instagram link={'https://www.instagram.com/shokzTV'} />
-                    </div>
-                </div>
-
-                <div className={'btn'} onClick={() => scrollTo('portfolio')}>
-                    Mehr über shokztv
-                </div>
-            </div>
+  return (
+    <div className={"heroBanner"}>
+      <div className={"inner"}>
+        <div className={"logo"}>
+          <Image
+            width={280}
+            src={"/images/herobanner_logo.png"}
+            height={120}
+            alt={"ShokzTV Logo"}
+          />
         </div>
 
-        <style jsx>{`
-            .heroBanner {
-                background-color: var(--blue);
-                padding: 5rem 0 4rem;
-            } 
+        <div className={"heroInfo"}>
+          <h1>shokzTV</h1>
+          <h2>Dota 2-Kommentatorenkanal</h2>
+          <div className={"socials"}>
+            <div className={"link twitter"}>
+              <Twitter link={"https://twitter.com/shokzTV"} />
+            </div>
+            <div className={"link twitch"}>
+              <Twitch link={"https://twitch.tv/shokzTV"} />
+            </div>
+            <div className={"link discord"}>
+              <Discord link={"https://discord.gg/hagYNWg"} />
+            </div>
+            <div className={"link instagram"}>
+              <Instagram link={"https://www.instagram.com/shokzTV"} />
+            </div>
+          </div>
 
-            .inner {
-                max-width: 1000px;
-                margin: 0 auto;
-                display: flex;
-                align-items: center;
-            }
+          <div className={"btn"} onClick={() => scrollTo("portfolio")}>
+            Mehr über shokztv
+          </div>
+        </div>
+      </div>
 
-            .logo {
-                padding: 0 4rem;
-            }
+      <style jsx>{`
+        .heroBanner {
+          background-color: var(--blue);
+          padding: 5rem 0 4rem;
+        }
 
-            h1 {
-                color: var(--orange);
-                margin: 0;
-                margin-bottom: .5rem;
-                font-size: 2rem;
-            }
+        .inner {
+          max-width: 1000px;
+          margin: 0 auto;
+          display: flex;
+          align-items: center;
+        }
 
-            h2 {
-                color: #FFF;
-                margin: 0;
-                font-weight: 400;
-                font-size: 1.25rem;
-            }
+        .logo {
+          padding: 0 4rem;
+        }
 
-            .socials {
-                display: flex;
-                align-items: center;
-                padding: 2rem 0;
-            }
+        h1 {
+          color: var(--orange);
+          margin: 0;
+          margin-bottom: 0.5rem;
+          font-size: 2rem;
+        }
 
-            .link {
-                cursor: pointer;
-            }
+        h2 {
+          color: #fff;
+          margin: 0;
+          font-weight: 400;
+          font-size: 1.25rem;
+        }
 
-            .twitter, .twitch, .discord {
-                margin-right: 2rem;
-            }
+        .socials {
+          display: flex;
+          align-items: center;
+          padding: 2rem 0;
+        }
 
-            .btn {
-                display: inline-flex;
-                color: #FFF;
-                border-radius: 2px;
-                border: 2px solid #FFF;
-                font-size: .8rem;
-                padding: .5rem 1rem;
-                text-transform: uppercase;
-                cursor: pointer;
-            }
+        .link {
+          cursor: pointer;
+        }
 
-            @media only screen and (max-width: 750px) {
-                .logo {
-                    display: none;
-                }
+        .twitter,
+        .twitch,
+        .discord {
+          margin-right: 2rem;
+        }
 
-                .heroInfo {
-                    padding: 0 3rem;
-                }
-            }
-        `}</style>
-    </div>;
+        .btn {
+          display: inline-flex;
+          color: #fff;
+          border-radius: 2px;
+          border: 2px solid #fff;
+          font-size: 0.8rem;
+          padding: 0.5rem 1rem;
+          text-transform: uppercase;
+          cursor: pointer;
+        }
+
+        @media only screen and (max-width: 750px) {
+          .logo {
+            display: none;
+          }
+
+          .heroInfo {
+            padding: 0 3rem;
+          }
+        }
+      `}</style>
+    </div>
+  );
 }
